79223d628b2bb2d5c4609d9188a1f0b1348d2097
Kai Kopecky <5694288+kkopecky@users.noreply.github.com>
2023-08-22 01:24:25 +0200

    Disable image optimization to fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Navbar from "@/components/Navbar/Navbar";
-import Hero from "@/components/Hero/Hero";
+import Navbar from '../components/Navbar/Navbar';
+import Hero from '../components/Hero/Hero';
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <div className="container mx-auto px-4">
-        <Hero />
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<Navbar />
+			<div className="container mx-auto px-4">
+				<Hero />
+			</div>
+		</main>
+	);
 }
